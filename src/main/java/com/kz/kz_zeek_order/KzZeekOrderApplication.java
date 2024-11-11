8cafedcab2fd11aa2972d424772e543bb1d81c4b
Kritsada Patchima <kissyoukissme93@gmail.com>
2024-11-11 10:27:30 +0700

    create project

---
+++ src/main/java/com/kz/kz_zeek_order/KzZeekOrderApplication.java
@@ -0,0 +1,13 @@
+package com.kz.kz_zeek_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KzZeekOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KzZeekOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kz/kz_zeek_order/KzZeekOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kz.kz_zeek_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KzZeekOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
